203a69d9fb98a9f05fa53f9d693897690ccb9c9b
ManuVleu <manu.vleurick@student.hogent.be>
2022-12-26 11:18:55 +0100

    Initial commit

---
+++ MADiOS_Weather_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MADiOS_Weather_App
+//
+//  Created by Manu Vleurick on 26/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
