45429eec7288eaad59373c4d37d3535c926cc1ac
ManuVleu <manu.vleurick@student.hogent.be>
2022-12-29 14:40:05 +0100

    fix

--- MADiOS_Weather_App/WeatherAPI.swift
+++ MADiOS_Weather_App/WeatherAPI.swift
@@ -1,27 +1,58 @@
 import Foundation
+import UIKit
 
-func fetchWeather(for city: City) {
+func fetchWeather(for city: City) -> Result<Weather, Error> {
     let apiKey = "50733048078f462e8fa115246220304"
     let urlString = "http://api.weatherapi.com/v1/forecast.json?key=\(apiKey)&g=\(city.name)&days=5&aqi=yes&alerts=no"
     guard let url = URL(string: urlString) else {
-        completion(.failure(APIError.invalidURL))
-        return
+        return .failure("Invalid URL" as! Error)
     }
+    
+    let semaphore = DispatchSemaphore(value: 0)
 
+    var result: Result<Weather, Error>!
     URLSession.shared.dataTask(with: url) { data, response, error in
         if let error = error {
-            completion(.failure(error))
-            return
+            result = .failure(error)
+        } else {
+            do {
+                let weatherData = try JSONDecoder().decode(WeatherJSON.self, from: data)
+                print(weatherData)
+                result = .success(weatherData)
+            } catch {
+                result = .failure(error)
+            }
         }
-
-        do {
-            let weatherData = try JSONDecoder().decode(Any.self, from: data)
-            print(weatherData)
-            completion(.success(()))
-        } catch {
-            completion(.failure(error))
-        }
+        semaphore.signal()
+        
     }.resume()
 
-    return weatherData
+    semaphore.wait()
+    return result
+}
+
+struct WeatherJSON: Decodable {
+    let current: Current
+    let location: Location
+    
+    struct Location: Decodable {
+        let localtime: String
+    }
+    
+    struct Current: Decodable {
+        let temp_c: Float
+        let condition: NSCondition
+        let wind_kph: Float
+        let wind_dir: String
+        let humidity: Int
+        let cloud: Int
+        
+        struct Condition: Decodable {
+            let text: String
+            let icon: String
+        }
+    }
+    //if attribute names differ from the JSON names gebruik CodingKeys
+    
+    
 }
